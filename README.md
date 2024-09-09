3977b82e2e7072335e35f4ddf56f74d0377719ca
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-09-09 17:21:04 -0300

    combine pt and en into one gradio

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 ---
-title: Mms Tts Eng Por Gradio
-emoji: 🔥
+title: mms-tts-eng gradio
+emoji: 📝💬
 colorFrom: blue
 colorTo: green
 sdk: gradio
-sdk_version: 4.43.0
+sdk_version: 4.42.0
 app_file: app.py
 pinned: false
 ---
-
-Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ app.py
@@ -0,0 +1,38 @@
+import gradio as gr
+import numpy as np
+
+from transformers import pipeline
+
+pipeline_en = pipeline(task="text-to-speech", model="facebook/mms-tts-eng")
+pipeline_pt = pipeline(task="text-to-speech", model="facebook/mms-tts-por")
+
+def tts(lang):
+  pipeline = pipeline_en if lang == "en" else pipeline_pt
+  def tts_lang(txt):
+    res = pipeline(txt)
+    audio = (res['audio'].reshape(-1) * 2 ** 15).astype(np.int16)
+    return res['sampling_rate'], audio
+  return tts_lang
+
+with gr.Blocks() as demo:
+  gr.Interface(
+    tts("en"),
+    inputs=gr.Textbox(
+      lines=1,
+      value="one two three four",
+    ),
+    outputs="audio",
+  )
+
+  gr.Interface(
+    tts("pt"),
+    inputs=gr.Textbox(
+      lines=1,
+      value="um dois tres quatro",
+    ),
+    outputs="audio",
+  )
+
+
+if __name__ == "__main__":
+   demo.launch()
